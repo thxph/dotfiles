623b79a59c38237d7edf4ed119fffe9152c0394f
Te[ss]a <thanh.x.pham@me.com>
2012-08-06 16:08:31 +0200

    * Make symlinks instead of printing out names * Make symlinks to oh-my-zsh custom folder at the end of the script

--- initialize.bash
+++ initialize.bash
@@ -4,12 +4,13 @@ set -e
 here="$(dirname "$0")"
 here="$(cd "$here"; pwd)"
 
-find "$here/oh-my-zsh-custom/custom" -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "oh-my-zsh/custom/"
-find "$here/oh-my-zsh-custom/themes" -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "oh-my-zsh/themes/"
-
 for file in "$here"/*; do
     name="$(basename "$file")"
     if [[ !( " initialize.bash oh-my-zsh-custom readme.md " =~ " $name " || -d $file/.git ) ]]; then
-        echo $file
+        ln -sfhv $file "$HOME/.$name"
     fi
 done
+
+find "$here/oh-my-zsh-custom/custom" -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "$HOME/oh-my-zsh/custom/"
+find "$here/oh-my-zsh-custom/themes" -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "$HOME/oh-my-zsh/themes/"
+
