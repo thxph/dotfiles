3784c9672c2de01c7936f0d3cdb06c39e5fc755e
pxt <phamx@eurecom.fr>
2014-07-22 11:10:26 +0700

    * Copy tmux buffer to clipboard with y.

--- initialize.bash
+++ initialize.bash
@@ -10,7 +10,7 @@ here="$(cd "$here"; pwd)"
 mkln () {
     for file in "$here"/"$1"*; do
         name="$(basename "$file")"
-        if [[ !( " config initialize.bash oh-my-zsh-custom readme.md " =~ " $name " ) ]]; then
+        if [[ !( " bin config initialize.bash oh-my-zsh-custom readme.md " =~ " $name " ) ]]; then
             if [[ $2 == 't' ]]; then
                 if [[ -e "$HOME/.$1$name" ]]; then
                     rm -rv "$HOME/.$1$name"
@@ -33,9 +33,20 @@ mkln () {
     done
 }
 
+chkln () {
+    for file in "$here/$1"*; do
+        name="$(basename "$file")"
+        if [[ -e "$HOME/$1$name" ]]; then
+            echo "  `dirname $HOME/$1$name`/`basename\
+            $HOME/$1$name`"
+        fi
+    done
+}
+
 echo "Conflicting dotfiles in $HOME:"
 mkln '' f
 mkln 'config/' f
+chkln 'bin/'
 echo -n "Type Y/y to overwrite those files: "
 read -n 1 c
 echo ''
@@ -45,14 +56,21 @@ if [[ $c == 'Y' ]] || [[ $c == 'y' ]]; then
         mkdir "$HOME/.config"
     fi
 
+    if [[ ! -d "$HOME/bin" ]]; then
+        echo "Creating ~/bin"
+        mkdir "$HOME/bin"
+    fi
+
     mkln '' t
     mkln 'config/' t
 
     if [[ `uname` == 'Linux' ]]; then
+        find "$here/bin" -maxdepth 1 -mindepth 1 -type f -print0 | xargs -0 -L 1 -I % ln -sfv % "$HOME/bin/"
         find "$here/oh-my-zsh-custom/custom" -maxdepth 1 -mindepth 1 -type f -print0 | xargs -0 -L 1 -I % ln -sfv % "$HOME/.oh-my-zsh/custom/"
         find "$here/oh-my-zsh-custom/themes" -maxdepth 1 -mindepth 1 -print0 | xargs -0 -L 1 -I % ln -sfv % "$HOME/.oh-my-zsh/themes/"
         find "$here/oh-my-zsh-custom/custom/plugins/" -maxdepth 1 -mindepth 1 -print0 | xargs -0 -L 1 -I % ln -sfv % "$HOME/.oh-my-zsh/custom/plugins/"
     elif [[ `uname` == 'Darwin' ]]; then
+        find "$here/bin" -type f -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "$HOME/bin"
         find "$here/oh-my-zsh-custom/custom" -type f -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "$HOME/.oh-my-zsh/custom/"
         find "$here/oh-my-zsh-custom/themes" -type f -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "$HOME/.oh-my-zsh/themes/"
         find "$here/oh-my-zsh-custom/custom/plugins" -depth 1 -print0 | xargs -0 -L 1 -I % ln -sfhv % "$HOME/.oh-my-zsh/custom/plugins/"
